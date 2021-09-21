62edff0046aa75b0fc1f184c4a00ec1dd38d904c
Stéphano TELOLAHY <stephano.telolahy@gmail.com>
2021-09-21 12:33:38 +0200

    discard all cards from eliminated

---
+++ test/engine/rules/discard_all_cards_on_eliminated_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wildwest_flutter/engine/event/event.dart';
+import 'package:wildwest_flutter/engine/rules/rules.dart';
+import 'package:wildwest_flutter/engine/setup/loader.dart';
+import 'package:wildwest_flutter/engine/state/state.dart';
+
+void main() {
+  late GRules sut;
+
+  setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    final abilities = await ResLoader().loadAbilities();
+    sut = GRules(abilities: abilities);
+  });
+
+  test('discard all cards on eliminated', () {
+    // Given
+    final player1 = GPlayer(
+        identifier: 'p1',
+        abilities: ['discardAllCardsOnEliminated'],
+        hand: [GCard(identifier: 'c1')],
+        inPlay: [GCard(identifier: 'c2')]);
+    final state = GState(players: [player1]);
+    final event = GEventEliminate(player: 'p1');
+
+    // When
+    final moves = sut.triggered(event, state);
+
+    // Assert
+    expect(moves, equals([GMove(ability: 'discardAllCardsOnEliminated', actor: 'p1')]));
+    final events = sut.effects(moves.first, state);
+    expect(
+        events,
+        equals([
+          GEventDiscardHand(player: 'p1', card: 'c1'),
+          GEventDiscardInPlay(player: 'p1', card: 'c2'),
+        ]));
+  });
+
+  test('do nothing if eliminated without cards', () {
+    // Given
+    final player1 = GPlayer(identifier: 'p1', abilities: ['discardAllCardsOnEliminated']);
+    final state = GState(players: [player1]);
+    final event = GEventEliminate(player: 'p1');
+
+    // When
+    final moves = sut.triggered(event, state);
+
+    // Assert
+    expect(moves, isEmpty);
+  });
+}
