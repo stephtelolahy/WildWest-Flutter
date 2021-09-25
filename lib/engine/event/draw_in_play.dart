813c7212851583d9f273fc39b91445405595ef11
Stéphano TELOLAHY <stephano.telolahy@gmail.com>
2021-09-25 16:56:11 +0200

    draw inPlay

--- lib/engine/event/draw_in_play.dart
+++ lib/engine/event/draw_in_play.dart
@@ -16,6 +16,13 @@ class GEventDrawInPlay extends GEvent {
 
   @override
   GState? dispatch(GState aState) {
-    throw UnimplementedError();
+    final state = GState.copy(aState);
+    final index = state.player(id: other).inPlay.indexWhere((e) => e.id == card);
+    final cardObject = state.player(id: other).inPlay.removeAt(index);
+    state.player(id: player).hand.add(cardObject);
+    return state;
   }
+
+  @override
+  Duration? duration() => DEFAULT_EVENT_DURATION;
 }
