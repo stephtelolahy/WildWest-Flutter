cc20ed20684987d3ad0fd15a9ad378bfb3b1ef3b
Stéphano TELOLAHY <stephano.telolahy@gmail.com>
2021-09-25 16:44:22 +0200

    draw hand

--- lib/engine/event/draw_hand.dart
+++ lib/engine/event/draw_hand.dart
@@ -16,6 +16,13 @@ class GEventDrawHand extends GEvent {
 
   @override
   GState? dispatch(GState aState) {
-    throw UnimplementedError();
+    final state = GState.copy(aState);
+    final index = state.player(id: other).hand.indexWhere((e) => e.id == card);
+    final cardObject = state.player(id: other).hand.removeAt(index);
+    state.player(id: player).hand.add(cardObject);
+    return state;
   }
+
+  @override
+  Duration? duration() => DEFAULT_EVENT_DURATION;
 }
