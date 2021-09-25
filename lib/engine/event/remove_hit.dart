237ab5174e3d08768dae0e7ce43712b5c36d7c82
Stéphano TELOLAHY <stephano.telolahy@gmail.com>
2021-09-25 08:10:17 +0200

    remove hit

--- lib/engine/event/remove_hit.dart
+++ lib/engine/event/remove_hit.dart
@@ -11,7 +11,30 @@ class GEventRemoveHit extends GEvent {
   List<Object?> get props => [player];
 
   @override
-  GState dispatch(GState state) {
-    throw UnimplementedError();
+  GState dispatch(GState aState) {
+    final hit = aState.hit;
+    if (hit == null) {
+      throw UnsupportedError('Missing hit');
+    }
+
+    final index = hit.players.indexOf(player);
+    if (index == -1) {
+      throw UnsupportedError('Player not found');
+    }
+
+    final state = GState.copy(aState);
+    hit.players.removeAt(index);
+
+    if (index < hit.targets.length) {
+      hit.targets.removeAt(index);
+    }
+
+    if (hit.players.isNotEmpty) {
+      state.hit = hit;
+    } else {
+      state.hit = null;
+    }
+
+    return state;
   }
 }
---
+++ test/engine/event/remove_hit_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wildwest_flutter/engine/event/event.dart';
+import 'package:wildwest_flutter/engine/state/state.dart';
+
+void main() {
+  test('update players if removing hit', () {
+    // Given
+    final state = GState(hit: GHit(players: ['p1', 'p2']));
+    final event = GEventRemoveHit(player: 'p1');
+
+    // When
+    final result = event.dispatch(state);
+
+    // Assert
+    expect(result.hit?.players, equals(['p2']));
+  });
+
+  test('update both targets and players if removing hit', () {
+    // Given
+    final state = GState(hit: GHit(players: ['p1', 'p1'], targets: ['p2', 'p3']));
+    final event = GEventRemoveHit(player: 'p1');
+
+    // When
+    final result = event.dispatch(state);
+
+    // Assert
+    expect(result.hit?.players, equals(['p1']));
+    expect(result.hit?.targets, equals(['p3']));
+  });
+
+  test('unset hit completely if removing hit', () {
+    // Given
+    final state = GState(hit: GHit(players: ['p1']));
+    final event = GEventRemoveHit(player: 'p1');
+
+    // When
+    final result = event.dispatch(state);
+
+    // Assert
+    expect(result.hit, isNull);
+  });
+}
